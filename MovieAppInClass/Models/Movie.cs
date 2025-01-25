180bbaea53a65b497e6981d09ade96cf3d9952d9
Ulisses Telemaco Neto <ulisses.telemaco@gmail.com>
2025-01-25 12:52:17 -0500

    Strint #1: Project Kick-off

---
+++ MovieAppInClass/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieAppInClass.Models
+{
+    public class Movie
+    {
+        //Eventually the PK
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public int Ratting { get; set; }
+
+    }
+}
